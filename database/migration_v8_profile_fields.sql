e45580c9fd3f77a3fd56ffe0a2604ed9481d4852
Verzadene <zehgenious@gmail.com>
2026-05-08 15:33:48 +0800

    feat: remove outdated migration scripts for database setup and versioning

--- database/migration_v8_profile_fields.sql
+++
@@ -1,8 +0,0 @@
--- FABulous Migration v8
--- Run via phpMyAdmin or: mysql -u root fab_ulous < migration_v8_profile_fields.sql
---
--- Changes:
---   1. Adds 'bio' column to the accounts table.
-
-ALTER TABLE accounts
-  ADD COLUMN IF NOT EXISTS bio VARCHAR(255) DEFAULT '';
